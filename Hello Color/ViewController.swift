915e2783519845c10c5f64525acca3a5c432c09b
Nan Su <nsu@salesforce.com>
2019-12-21 16:58:05 -0500

    Initial Commit

---
+++ Hello Color/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Color
+//
+//  Created by Nan Su on 12/21/19.
+//  Copyright © 2019 Nan Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
